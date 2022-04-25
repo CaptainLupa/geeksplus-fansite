52d38f1cb0e516a7eff4678de54aeca7f729e9ba
Jens Eckert <capt.lupa@gmail.com>
2022-04-25 13:21:31 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="flex flex-row justify-center">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <head>
+        <h1>We are gaming!</h1>
+      </head>
+    );
+  }
+}
