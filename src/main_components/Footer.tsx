c01268f44f18436b3f9129fba229a61a912e152d
Jens Eckert <capt.lupa@gmail.com>
2022-04-26 14:56:24 -0400

    Functionalized influencer list

--- src/main_components/Footer.tsx
+++ src/main_components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 class Footer extends React.Component {
   render() {
     return (
-      <footer className=" flex flex-row justify-center bg-green-500">
+      <footer className=" flex flex-row justify-center bg-green-500 h-16">
         <h1>Not affiliated with Geex+ I'm just making this for fun.</h1>
       </footer>
     );
--- src/minor_components/Influencers.tsx
+++ src/minor_components/Influencers.tsx
@@ -5,6 +5,76 @@ import joey from "../static/influencer_animeman.png";
 import garnt from "../static/influencer_gigguk.png";
 import sydney from "../static/influencer_sydsnap.png";
 import foxen from "../static/influencer_foxenanime.png";
+import trashTaste from "../static/influencer_trashtaste.png";
+import basement from "../static/influencer_mothers.png";
+
+interface InfluencerGridInfo {
+  link: string;
+  image: any;
+  name: string;
+}
+
+const InfluencerListItem = (props: InfluencerGridInfo) => {
+  return (
+    <li>
+      <a href={props.link} className="flex flex-col items-center">
+        <div className="pb-10">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <p className="whitespace-nowrap">{props.name}</p>
+      </a>
+    </li>
+  );
+};
+
+const InfluencersJapan = () => {
+  return (
+    <ul className="grid grid-cols-4 gap-[7%] text-center font-normal pb-10 pt-10">
+      <InfluencerListItem
+        link="https://www.youtube.com/c/CDawgVA"
+        image={connor}
+        name="CDawgVA"
+      />
+      <InfluencerListItem
+        link="https://www.youtube.com/c/animezone"
+        image={garnt}
+        name="Gigguk"
+      />
+      <InfluencerListItem
+        link="https://www.youtube.com/c/TheAnimeMan"
+        image={joey}
+        name="The Anime Man"
+      />
+      <InfluencerListItem
+        link="https://www.youtube.com/c/sydsnap"
+        image={sydney}
+        name="Sydsnap"
+      />
+      <InfluencerListItem
+        link="https://www.youtube.com/channel/UCzs3FmQ5qPfBcZj-xSZDKcw"
+        image={foxen}
+        name="Foxen"
+      />
+      <InfluencerListItem
+        link="https://www.youtube.com/c/TrashTaste"
+        image={trashTaste}
+        name="Trash Taste"
+      />
+    </ul>
+  );
+};
+
+const InfluencersOverseas = () => {
+  return (
+    <ul className="grid grid-flow-col gap-[7%] text-center font-normal pb-10 pt-10">
+      <InfluencerListItem
+        link="https://youtube.com/c/MothersBasement"
+        image={basement}
+        name="Mother's Basement"
+      />
+    </ul>
+  );
+};
 
 const Influencers = () => {
   return (
@@ -14,63 +84,9 @@ const Influencers = () => {
       </h2>
       <div className="flex flex-col items-center">
         <h2>Japan</h2>
-        <ul className="grid grid-cols-4 gap-[7%] text-3xl text-center font-normal pb-10 pt-10">
-          <li>
-            <a
-              href="https://www.youtube.com/c/CDawgVA"
-              className="flex flex-col items-center"
-            >
-              <div className="pb-10">
-                <img src={connor} alt="CDawgVA" />
-              </div>
-              <p>CDawgVA</p>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/c/animezone"
-              className="flex flex-col items-center"
-            >
-              <div className="pb-10">
-                <img src={garnt} />
-              </div>
-              <p>Gigguk</p>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/c/TheAnimeMan"
-              className="flex flex-col items-center"
-            >
-              <div className="pb-10">
-                <img src={joey} />
-              </div>
-              <p>The Anime Man</p>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/c/sydsnap"
-              className="flex flex-col items-center"
-            >
-              <div className="pb-10">
-                <img src={sydney} />
-              </div>
-              <p>Sydney</p>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/channel/UCzs3FmQ5qPfBcZj-xSZDKcw"
-              className="flex flex-col items-center"
-            >
-              <div className="pb-10">
-                <img src={foxen} />
-                <p>Foxen Anime</p>
-              </div>
-            </a>
-          </li>
-        </ul>
+        <InfluencersJapan />
+        <h2 className="pt-10">Overseas</h2>
+        <InfluencersOverseas />
       </div>
     </div>
   );
