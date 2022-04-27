8a3542612ddd9e129b02d21a0ce76214baa0331b
Jens Eckert <capt.lupa@gmail.com>
2022-04-26 22:27:25 -0400

    Influencers stuff

--- src/minor_components/Influencers.tsx
+++ src/minor_components/Influencers.tsx
@@ -12,84 +12,83 @@ interface InfluencerGridInfo {
   link: string;
   image: any;
   name: string;
+  css?: string;
 }
 
-const InfluencerListItem = (props: InfluencerGridInfo) => {
-  return (
-    <li>
-      <a href={props.link} className="flex flex-col items-center">
-        <div className="pb-10">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <p className="whitespace-nowrap">{props.name}</p>
-      </a>
-    </li>
-  );
-};
-
-const InfluencersJapan = () => {
-  return (
-    <ul className="grid grid-cols-4 gap-[7%] text-center font-normal pb-10 pt-10">
-      <InfluencerListItem
-        link="https://www.youtube.com/c/CDawgVA"
-        image={connor}
-        name="CDawgVA"
-      />
-      <InfluencerListItem
-        link="https://www.youtube.com/c/animezone"
-        image={garnt}
-        name="Gigguk"
-      />
-      <InfluencerListItem
-        link="https://www.youtube.com/c/TheAnimeMan"
-        image={joey}
-        name="The Anime Man"
-      />
-      <InfluencerListItem
-        link="https://www.youtube.com/c/sydsnap"
-        image={sydney}
-        name="Sydsnap"
-      />
-      <InfluencerListItem
-        link="https://www.youtube.com/channel/UCzs3FmQ5qPfBcZj-xSZDKcw"
-        image={foxen}
-        name="Foxen"
-      />
-      <InfluencerListItem
-        link="https://www.youtube.com/c/TrashTaste"
-        image={trashTaste}
-        name="Trash Taste"
-      />
-    </ul>
-  );
-};
-
-const InfluencersOverseas = () => {
-  return (
-    <ul className="grid grid-flow-col gap-[7%] text-center font-normal pb-10 pt-10">
-      <InfluencerListItem
-        link="https://youtube.com/c/MothersBasement"
-        image={basement}
-        name="Mother's Basement"
-      />
-    </ul>
-  );
-};
-
-const Influencers = () => {
-  return (
-    <div className="font-bold flex flex-col items-center pt-10 text-5xl">
-      <h2 className="pb-10">
-        インフルエンサー <span className="text-4xl">-Influencers-</span>
-      </h2>
-      <div className="flex flex-col items-center">
-        <h2>Japan</h2>
-        <InfluencersJapan />
-        <h2 className="pt-10">Overseas</h2>
-        <InfluencersOverseas />
+const InfluencerListItem = ({
+  link,
+  image,
+  name,
+  css = "",
+}: InfluencerGridInfo): JSX.Element => (
+  <li>
+    <a href={link} className="flex flex-col items-center">
+      <div className="pb-10">
+        <img src={image} alt={name} />
       </div>
+      <p className={css}>{name}</p>
+    </a>
+  </li>
+);
+
+const InfluencersJapan = (): JSX.Element => (
+  <ul className="grid grid-cols-4 gap-[7%] text-center font-normal pb-10 pt-10">
+    <InfluencerListItem
+      link="https://www.youtube.com/c/CDawgVA"
+      image={connor}
+      name="CDawgVA"
+    />
+    <InfluencerListItem
+      link="https://www.youtube.com/c/animezone"
+      image={garnt}
+      name="Gigguk"
+    />
+    <InfluencerListItem
+      link="https://www.youtube.com/c/TheAnimeMan"
+      image={joey}
+      name="The Anime Man"
+      css="whitespace-nowrap"
+    />
+    <InfluencerListItem
+      link="https://www.youtube.com/c/sydsnap"
+      image={sydney}
+      name="Sydsnap"
+    />
+    <InfluencerListItem
+      link="https://www.youtube.com/channel/UCzs3FmQ5qPfBcZj-xSZDKcw"
+      image={foxen}
+      name="Foxen"
+    />
+    <InfluencerListItem
+      link="https://www.youtube.com/c/TrashTaste"
+      image={trashTaste}
+      name="Trash Taste"
+    />
+  </ul>
+);
+
+const InfluencersOverseas = (): JSX.Element => (
+  <ul className="grid grid-flow-col gap-[7%] text-center font-normal pb-10 pt-10">
+    <InfluencerListItem
+      link="https://youtube.com/c/MothersBasement"
+      image={basement}
+      name="Mother's Basement"
+    />
+  </ul>
+);
+
+const Influencers = (): JSX.Element => (
+  <div className="font-bold flex flex-col items-center pt-10 text-5xl">
+    <h2 className="pb-10">
+      インフルエンサー <span className="text-4xl">-Influencers-</span>
+    </h2>
+    <div className="flex flex-col items-center">
+      <h2>Japan</h2>
+      <InfluencersJapan />
+      <h2 className="pt-10">Overseas</h2>
+      <InfluencersOverseas />
     </div>
-  );
-};
+  </div>
+);
 
 export default Influencers;
